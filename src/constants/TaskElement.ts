bef117ac245862b97b4228de958e140996a3a54b
kosu <kosumon27@gmail.com>
2022-07-26 04:53:35 +0900

    ⚡️ Task

---
+++ src/constants/TaskElement.ts
@@ -0,0 +1,28 @@
+import { TaskElement } from "src/type/type";
+
+export const taskElement: TaskElement[] = [
+  {
+    id: 1,
+    header: "今日する",
+    color: "text-today",
+    bgColor: "checked:bg-today",
+    caretColor: "caret-today",
+    day: "today",
+  },
+  {
+    id: 2,
+    header: "明日する",
+    color: "text-tomorrow",
+    bgColor: "checked:bg-tomorrow",
+    caretColor: "caret-tomorrow",
+    day: "tomorrow",
+  },
+  {
+    id: 3,
+    header: "今度する",
+    color: "text-other",
+    bgColor: "checked:bg-other",
+    caretColor: "caret-other",
+    day: "other",
+  },
+];
