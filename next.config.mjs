b91863c4bb28f824c24d34de82d9fd8af2f3349d
kosu <kosumon27@gmail.com>
2022-07-28 18:01:34 +0900

    🩹

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,11 @@
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-
-if (!SUPABASE_URL) {
-  throw new Error("環境変数が未定義 : env.NEXT_PUBLIC_SUPABASE_URL");
-}
-
-const DOMAIN = SUPABASE_URL.split("/").slice(-1)[0];
-
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   i18n: { locales: ["ja"], defaultLocale: "ja" },
-  future: { strictPostcssConfiguration: true },
-  reactStrictMode: true,
   poweredByHeader: false,
+  reactStrictMode: true,
   swcMinify: true,
-  // experimental: { concurrentFeatures: true, serverComponents: true },
-  images: {
-    domains: [DOMAIN],
-  },
 };
 
 export default nextConfig;
