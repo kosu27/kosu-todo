12ccd95eb356faa7ed72713e5db27826489ca711
kosu <kosumon27@gmail.com>
2022-07-28 17:56:04 +0900

    🔧

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        today: "#F43F5E",
+        tomorrow: "#FB923C",
+        other: "#FBBF24",
+        darkhover: "#2D333B",
+        darkbg: "#22272E",
+      },
+    },
+  },
   plugins: [
-    require("@tailwindcss/forms"),
+    require("daisyui"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
   ],
+  darkMode: "class",
 };
