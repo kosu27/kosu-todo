093c9172c520855f3f1171de0c9d0b10069d8a24
kosu <kosumon27@gmail.com>
2022-07-28 18:50:42 +0900

    🐛

--- src/lib/SupabaseClient.ts
+++ src/lib/SupabaseClient.ts
@@ -6,7 +6,15 @@ import { getDate, getDateEnd } from "src/lib/Datetime";
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+if (!SUPABASE_URL) {
+  throw new Error("環境変数が未定義 : env.NEXT_PUBLIC_SUPABASE_URL");
+}
+if (!SUPABASE_ANON_KEY) {
+  throw new Error("環境変数が未定義 : env.NEXT_PUBLIC_SUPABASE_KEY");
+}
+
 export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 export type TodoType = {
   id: number;
   uid: string;
