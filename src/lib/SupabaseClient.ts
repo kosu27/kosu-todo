78fcfb33090bf7631a06941894f3f35c165da3c7
kosu <kosumon27@gmail.com>
2022-07-28 18:47:36 +0900

    🩹

--- src/lib/SupabaseClient.ts
+++ src/lib/SupabaseClient.ts
@@ -7,7 +7,6 @@ const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 export type TodoType = {
   id: number;
   uid: string;
